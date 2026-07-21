15e668f467d508118ba7a1684a722a7a92fc85b2
BoB <710850609@qq.com>
2026-07-21 22:13:39 +0800

    升级py3.11工具链

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,8 +1,8 @@
 // Top-level build file for EasyTier-EUI Android
 plugins {
-    id("com.android.application") version "8.2.0" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.20" apply false
-    id("com.chaquo.python") version "15.0.1" apply false
+    id("com.android.application") version "8.4.0" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
+    id("com.chaquo.python") version "16.0.0" apply false
 }
 
 tasks.register("clean", Delete::class) {
