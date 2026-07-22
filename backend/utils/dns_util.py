33a05e18b9d605fd4ec71f9cf4edd519e59ee1cd
BoB <710850609@qq.com>
2026-07-22 09:31:35 +0800

    up

--- backend/utils/dns_util.py
+++ backend/utils/dns_util.py
@@ -2,11 +2,32 @@
 # -*- coding: utf-8 -*-
 
 import dns.resolver
+import os
+import subprocess
+
+# Android 没有 /etc/resolv.conf ，会初始化报错
+if os.path.isfile('/etc/resolv.conf'):
+    resolver = dns.resolver.get_default_resolver()
+else:
+    resolver = dns.resolver.Resolver()
+    nameservers = []
+    try:
+        for key in ['net.dns1', 'net.dns2', 'net.dns3', 'net.dns4']:
+            result = subprocess.run(['getprop', key], capture_output=True, text=True, timeout=2)
+            ns = result.stdout.strip()
+            if ns:
+                nameservers.append(ns)
+    except Exception:
+        pass
+    if not nameservers:
+        nameservers = ['114.114.114.114', '223.5.5.5', '119.29.29.29']
+    resolver.nameservers = nameservers
+
 
 def get_dns_txt_records(domain, timeout=5):
     """获取域名的所有 TXT 记录"""
     try:
-        answers = dns.resolver.resolve(domain, 'TXT', lifetime=timeout)
+        answers = resolver.resolve(domain, 'TXT', lifetime=timeout)
         records = []
         for rdata in answers:
             # TXT 记录可能包含多个字符串段，拼接起来
